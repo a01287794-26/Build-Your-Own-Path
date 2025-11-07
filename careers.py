f778112d9532c7d5e84d88e8cbdc765f59d0749d
Daniela Garcia Uribe <149635074+DanyGarciaUribe@users.noreply.github.com>
2025-11-07 13:07:13 -0600

    Add educational areas and print example

--- careers.py
+++ careers.py
@@ -127,3 +127,69 @@ while True:
             print("\033[1;37mDid you mean:\033[0m")
             for s in suggestions[:10]:
                 print(f"  - {s}")
+areas={
+    "Negocios":[
+        "Licenciatura en Contaduria Publica y Finanzas",
+        "Licenciatura en Desarrollo de Talento y Cultura Organizacional", 
+        "Licenciatura en Emprendimiento",
+        "Licenciatura en Estrategia y Transformacion de Negocios", 
+        "Licenciatura en Inteligencia de Negocios",
+        "Licenciatura en Mercadoctenia",
+        "Licenciatura en Negocios Internacionales"
+        ],
+    "Salud":[
+        "Licenciatura en Biociencias",
+        "Licenciatura en Nutricion y Bienestar Integral", 
+        "Licenciatura en Psicologia Clinica y de la Salud",
+        "Medico Cirujano", 
+        "Medico Cirujano Odontologo"
+        ],
+    "Estudios Creativos":[
+        "Arquitectura",
+        "Licenciatura en Comunicacion", 
+        "Licenciatura en Design", 
+        "Licenciatura en Letras Hispanicas",
+        "Licenciatura en Tecnologia y Produccion Musical", 
+        "Licenciatura en Arte Digital", 
+        "Licenciatura en Innovacion Educativa"
+        ],
+    "Ambiente Construido":[
+        "Arquitectura", 
+        "Ingenieria Civil",
+        "Licenciatura en Urbanismo"
+        ],
+    "Derecho, Economia y Relaciones Internacionales":[
+        "Licenciatura en Derecho", 
+        "Licenciatura en Economia", 
+        "Licenciatura en Gobierno y Transformacion Publica", 
+        "Licenciatura en Relaciones Internacionales"
+        ],
+    "Innovacion y Transformacion": [
+        "Ingenieria Biomedica",
+        "Ingenieria Civil",
+        "Ingenieria en Electronica",
+        "Ingenieria Industrial y de Sistemas", 
+        "Ingenieria en Innovacion y Desarrollo",
+        "Ingenieria Mecanica", 
+        "Ingenieria en Mecatronica"
+        ],
+    "Computacion y Tecnologias de Informacion": [
+        "Ingenieria en Robotica y Sistemas Digitales", 
+        "Ingenieria en Tecnologias Computacionales",
+        "Ingenieria en Transformacion Digital de Negocios"
+       ],
+     "Bioingenieria y Procesos Quimicos": [
+        "Ingenieria en Alimentos" ,
+        "Ingenieria en Biosistemas Agroalimentarios",
+        "Ingenieria en Biotecnologia",
+        "Ingenieria en Desarrollo Sustentable",
+        "Ingenieria Quimica"
+     ],
+      "Ciencias Aplicadas": [
+        "Ingenieria en Ciencia de Datos y Matematicas" ,
+        "Ingenieria en Fisica Industrial", 
+        "Ingenieria en Nanotecnologia"
+         ],
+    }
+
+print(areas["Negocios"])
